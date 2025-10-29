3ba35c6b52ee80d59796c13b071c5e6e5a317828
Kajal Rajak <167600142+KajalR4321@users.noreply.github.com>
2025-10-29 14:31:03 +0530

    Add files via upload
    Started JavaScript learning journey with variables

---
+++ variable.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+console.log("hello I am Kajal");
+console.log("I have recently passed out");
+num=90;
+console.log(num);
+let no1=30;
+console.log(typeof no1);
+//type coreision 
+let addition ="1"+2
+//2 string convert
+console.log(addition)
+//numeric conversion
+let sum ="1"*2;
+console.log(sum);
